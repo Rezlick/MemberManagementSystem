16cefdf4ba657291fb01084ad6a031ed416bf5e4
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-07-29 18:43:16 +0700

    create interfaces

---
+++ Frontend/src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface UsersInterface {
+    ID?: number;
+    FirstName?: string;
+    LastName?: string;
+    Email?: string;
+    PhoneNumber?: string;
+    Age?: number;
+    BirthDay?: string;
+    RoleID?: number;
+    Password?: string;
+  }
